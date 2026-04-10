befc862c8a18af0761f42c6924ce4daeeaa8ca21
Yuvraj <yuvrajadwal2000@gmail.com>
2026-04-10 18:52:48 +0530

    change cloudinary connected function

--- server/server.js
+++ server/server.js
@@ -14,9 +14,10 @@ let isCloudinaryConnected = false;
 app.use(cors())
 app.use(express.json())
 app.use(clerkMiddleware())
-
-
 app.use(requireAuth())
+
+// await connectCloudinary();
+
 app.use(async (req, res, next) => {
     if (!isCloudinaryConnected) {
         await connectCloudinary();
@@ -27,6 +28,9 @@ app.use(async (req, res, next) => {
 app.use("/api/status", (req, res) => res.send("Server is live"))
 app.use('/api/ai', aiRouter)
 app.use('/api/user', userRouter)
+
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => console.log("Server is running on PORT:", port))
 
 if (process.env.NODE_ENV !== "production") {
     const port = process.env.PORT || 5000;
