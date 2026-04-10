14288d31927ec67457fd52789decda27f701c8b5
Yuvraj <yuvrajadwal2000@gmail.com>
2026-04-10 20:20:24 +0530

    remove resume review

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { auth } from '../middlewares/auth.js'; 
-import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject, resumeReview } from '../controllers/aiController.js';
+import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject } from '../controllers/aiController.js';
 import { upload } from '../configs/multer.js';
 
 const aiRouter = express.Router()
